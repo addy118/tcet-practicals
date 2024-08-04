5fa34373251eb1fe7c6c4e976569fbc43601265a
addy <addyyy118@gmail.com>
2024-08-04 19:15:19 +0530

    PS-IV 1, 2

--- cloud-computing/exp-1/cloud-project/src/App.jsx
+++ cloud-computing/exp-1/cloud-project/src/App.jsx
@@ -16,15 +16,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>This is PS-IV Experiment 1</h1>
+      {/*<div className="card">*/}
+      {/*  <h1>This is PS-IV Experiment 1</h1>*/}
+      {/*</div>*/}
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
