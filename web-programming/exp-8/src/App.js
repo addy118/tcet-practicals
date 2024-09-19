c81cde8e4655a918e075a33308527631d0dfb245
Addy <addyyy118@gmail.com>
2024-09-19 11:18:57 +0530

    WP 8 Code

---
+++ web-programming/exp-8/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Here are the products that we sell:</h1>
+			<ul>
+				<li>Electronics</li>
+				<li>Fashion</li>
+				<li>Home Appliances</li>
+				<li>Furniture</li>
+				<li>Beauty & Personal Care</li>
+				<li>Sports & Fitness</li>
+				<li>Books & Stationary</li>
+				<li>Groceries & Essentials</li>
+				<li>Toys & Games</li>
+				<li>Automative Accessories</li>
+			</ul>
+		</div>
+	);
+}
+
+export default App;
