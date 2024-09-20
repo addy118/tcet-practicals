6a7489e792f9feb5ab2ed31e678d44ff83896aff
addy <addyyy118@gmail.com>
2024-09-20 18:13:23 +0530

    Cleaning

--- web-programming/sum.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $num1 = 3;
-    $num2 = 2;
-    $sum = $num1 + $num2
-    echo "Sum of {$num1} and {$num2} is {$num1 + $num2}"
-?>
-
-// i want to print "sum of (num1 value) and (num2 value) is (sum value)"
